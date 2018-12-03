bc2ad878c005d5caa42a6b3e7ea35c37d6f31bdd
wangchongwen <1291184157@qq.com>
2018-12-03 14:11:11 +0800

    提交 Signed-off-by: wangchongwen <1291184157@qq.com>

---
+++ src/helloworlds/HelloWorlds.java
@@ -0,0 +1,37 @@
+
+    /**  
+    * @Title: HelloWorlds.java
+    * @Package helloworlds
+    * @Description: TODO(用一句话描述该文件做什么)
+    * @author AMITUOFO
+    * @date 2018年12月3日
+    * @version V1.0  
+    */
+    
+package helloworlds;
+
+
+    /**
+    * @ClassName: HelloWorlds
+    * @Description: TODO(这里用一句话描述这个类的作用)
+    * @author AMITUOFO
+    * @date 2018年12月3日
+    *
+    */
+
+public class HelloWorlds {
+
+	/**
+	    * @Title: main
+	    * @Description: TODO(这里用一句话描述这个方法的作用)
+	    * @param @param args    参数
+	    * @return void    返回类型
+	    * @throws
+	    */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("你好，世界");
+	}
+
+}
